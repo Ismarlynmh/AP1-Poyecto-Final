c8d895f306f69da81ad65946a871c07f612a5a78
Ismarlin Mendoza Hernandez <ismarlyn123@gmail.com>
2021-11-20 14:19:40 -0400

    Agregando registro de empleados

--- UI/Registros/rEmpleados.xaml.cs
+++ UI/Registros/rEmpleados.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
 namespace Prueba_Ismarlin_Proyecto.UI.Registros
-{
+{ 
     /// <summary>
     /// Lógica de interacción para rEmpleados.xaml
     /// </summary>
