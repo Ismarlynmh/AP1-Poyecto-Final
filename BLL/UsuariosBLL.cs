b6b240c7129db6711137fdf9503ec87e3d77691d
Julio Cesar Jimeno Disla <julio_jimeno@ucne.edu.do>
2021-11-21 18:03:13 -0400

    Creacion de UsuarioBLL

---
+++ BLL/UsuariosBLL.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using Prueba_Ismarlin_Proyecto.DAL;
+using Prueba_Ismarlin_Proyecto.Entidades;
+
+
+namespace Prueba_Ismarlin_Proyecto.BLL
+{
+   public class UsuariosBLL
+    {
+        public static bool Guardar(Usuarios usuarios)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                if (contexto.Usuarios.Add(usuarios) != null)
+                    paso = (contexto.SaveChanges() > 0);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Modificar(Usuarios usuarios)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                contexto.Entry(usuarios).State = EntityState.Modified;
+                paso = (contexto.SaveChanges() > 0);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                var eliminar = contexto.Usuarios.Find(id);
+                contexto.Entry(eliminar).State = EntityState.Deleted;
+                paso = (contexto.SaveChanges() > 0);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static Usuarios Buscar(int id)
+        {
+            Usuarios usuario = new Usuarios();
+            Contexto contexto = new Contexto();
+            try
+            {
+                usuario = contexto.Usuarios.Find(id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return usuario;
+        }
+
+        public static List<Usuarios> GetList(Expression<Func<Usuarios, bool>> usuario)
+        {
+            List<Usuarios> lista = new List<Usuarios>();
+            Contexto contexto = new Contexto();
+            try
+            {
+                lista = contexto.Usuarios.Where(usuario).ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+    }
+}
